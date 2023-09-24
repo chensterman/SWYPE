342fccd713779164c0b7859bcb44743d31a46353
Leon Chen <lrchen01@gmail.com>
2023-09-23 20:39:37 -0400

    extension update

--- extension/rewards.js
+++ extension/rewards.js
@@ -3,27 +3,53 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 const checkBalance = document.getElementById("checkBalance");
 const rewardsBalance = document.getElementById("rewardsBalance");
+const redeemBalance = document.getElementById("redeemBalance");
+const redeem = document.getElementById("redeem");
+const redeemStatus = document.getElementById("redeemStatus");
 
 const apiBaseUrl = "https://swype-api.onrender.com/";
-const getBalanceBranch = "get_rewards_balance"
-const redeemBranch = "redeem_rewards"
+const startTaskBranch = "start_task"
 const taskStatusBranch = "check_task_status/"
 
-checkBalance.addEventListener('click', async () => {
-    rewardsBalance.innerHTML = "Checking your rewards balance, this make take a minute..."
-    var taskId = await startTaskGetBalance();
-    var data = await checkTaskStatus(taskId);
-    if (data !== null) {
-        rewardsBalance.innerHTML = `Rewards Balance: ${data}`;
-    } else {
-        rewardsBalance.innerHTML = "An error occurred. Please try again later."
-    }
-})
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+if (checkBalance) {
+    checkBalance.addEventListener('click', async () => {
+        console.log("here1");
+        rewardsBalance.innerHTML = "Checking your rewards balance, this make take a minute..."
+        var taskId = await startTaskGetBalance();
+        console.log(taskId);
+        var data = await checkTaskStatus(taskId);
+        console.log(data);
+        if (data !== null) {
+            rewardsBalance.innerHTML = `Rewards Balance: $${data.rewards_balance}`;
+        } else {
+            rewardsBalance.innerHTML = "An error occurred. Please try again later."
+        }
+    })
+}
+
+if (redeem) {
+    redeem.addEventListener('click', async () => {
+        rewardsBalance.innerHTML = "Redeeming your rewards, this make take a minute..."
+        var data = await startTaskRedeemRewards();
+        if (data !== null) {
+            rewardsBalance.innerHTML = `Successfully redeemed $${redeemBalance.value}!`;
+        } else {
+            rewardsBalance.innerHTML = "An error occurred. Please try again later."
+        }
+    })
+}
 
 // Begin a task to check rewards balance
 async function startTaskGetBalance() {
-    const getBalanceUrl = apiBaseUrl + getBalanceBranch
-    fetch(getBalanceUrl)
+    const getBalanceUrl = apiBaseUrl + startTaskBranch;
+    fetch(getBalanceUrl, {
+            method: "POST",
+            body:{"bank": bank.value, "task_type": "check_balance", "username": username.value, "password":password.value}
+        })
         .then(response => response.json())
         .then(data => {
             console.log(data);
